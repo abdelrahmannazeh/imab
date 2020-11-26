d34c2d2fafd3f54aaab37e1507a7db5b16fc02b4
abdelrahmannazeh <abdelrahmannazeh0@gmail.com>
2020-11-26 18:25:28 +0200

    First Commit

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+ static String id ='registeration_screen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Container(
+          alignment: Alignment.center,
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                  child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFeildDecoration.copyWith(labelText: 'Enter Your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration:kTextFeildDecoration.copyWith(labelText: 'Conferm Your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration:kTextFeildDecoration.copyWith(labelText: 'Enter Your Password'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFeildDecoration.copyWith(labelText: 'Conferm Your Password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+             RoundedButton(title: 'Register',color: Colors.blueAccent,onpressed: (){}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,72 @@
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+ static String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller =AnimationController(duration: Duration(seconds: 1),vsync: this);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {
+        
+      });
+      print(controller.value);
+     });
+  }
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(controller.value),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                    child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text:['IMAB'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(color: Colors.lightBlueAccent,title: 'Log In',onpressed:  () {
+                    Navigator.pushNamed(context, LoginScreen.id);},),
+            RoundedButton(color: Colors.blueAccent,title: 'Register',onpressed:  () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);},
+                    ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
